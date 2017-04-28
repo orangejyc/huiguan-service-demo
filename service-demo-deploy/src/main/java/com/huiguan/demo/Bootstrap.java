ca6b22ce2866e40499d555ff6cda52a77b126208
鞠永成 <ccloveni@163.com>
2017-04-29 01:58:04 +0800

    1.feat 增加样例配置

--- service-demo-deploy/src/main/java/com/huiguan/demo/Bootstrap.java
+++ service-demo-deploy/src/main/java/com/huiguan/demo/Bootstrap.java
@@ -4,6 +4,8 @@ import com.alibaba.boot.dubbo.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.CountDownLatch;
+
 /**
  * Created by ansoya on 2017/4/28.
  */
@@ -11,6 +13,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableDubboConfiguration
 public class Bootstrap {
     public static void main(String[] args) {
-        SpringApplication.run(Bootstrap.class, args);
+        try {
+            SpringApplication.run(Bootstrap.class, args);
+            CountDownLatch latch = new CountDownLatch(10);
+            latch.await();
+        }catch(Exception ex){
+
+        }
+
     }
 }
