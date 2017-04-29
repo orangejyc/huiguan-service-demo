cec173fe00a58a79104bcc9af3a1890efb47cbd2
鞠永成 <ccloveni@163.com>
2017-04-29 13:24:37 +0800

    1.fix sonar報警

--- service-demo-deploy/src/main/java/com/huiguan/demo/Bootstrap.java
+++ service-demo-deploy/src/main/java/com/huiguan/demo/Bootstrap.java
@@ -1,8 +1,6 @@
 package com.huiguan.demo;
 
 import com.alibaba.boot.dubbo.annotation.EnableDubboConfiguration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,8 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @EnableDubboConfiguration
 public class Bootstrap {
-    private static final Logger log = LoggerFactory.getLogger(Bootstrap.class);
-
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Bootstrap.class, args);
     }
