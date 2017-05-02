b0279a331108650abf795c91b4e94d531a8600a2
鞠永成 <ccloveni@163.com>
2017-05-02 21:45:15 +0800

    1.feat 模块重新划分

---
+++ service-demo-api/src/main/java/com/huiguan/demo/api/dto/UserDTO.java
@@ -0,0 +1,26 @@
+/*
+ * huiguan.com Inc.
+ * Copyright (c) 2017…… All Rights Reserved.
+ */
+
+package com.huiguan.demo.api.dto;
+
+import java.io.Serializable;
+
+/**
+ * 对外暴露实体类
+ *
+ * @author juyongcheng
+ * @since $$Revision:1.0.0, $$Date: 2017/4/28 09:36 $$
+ */
+public class UserDTO implements Serializable {
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
